b3b8470595e0ac7656e0a8fe5f2ca554471e7880
Anup Pradhan <anuppradhan929@gmail.com>
2025-05-01 07:16:03 +0530

    feat: create subscription model for user channel subscriptions

---
+++ src/models/subscription.model.js
@@ -0,0 +1,14 @@
+
+
+
+import mongoose, { Schema } from "mongoose";
+
+const subsciption = new Schema({
+    subscriber: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    channel: {type: Schema.Types.ObjectId, ref: 'User', required: true}
+
+
+},{timestamps: true})
+
+
+export const Subsciption = mongoose.model('Subsciption', subsciption)
